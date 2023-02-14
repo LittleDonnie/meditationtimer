605e6552932113aadc6dfdf7701cd74fcc5d28ab
Monty Montgomery <poweredbyplants@gmail.com>
2023-02-14 12:07:23 -0600

    Initial Commit

---
+++ Timer with Mod/Timer_with_ModApp.swift
@@ -0,0 +1,17 @@
+//
+//  Timer_with_ModApp.swift
+//  Timer with Mod
+//
+//  Created by Donald Montgomery on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Timer_with_ModApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Timer with ModUITests/Timer_with_ModUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Timer_with_ModUITestsLaunchTests.swift
+//  Timer with ModUITests
+//
+//  Created by Donald Montgomery on 2/14/23.
+//
+
+import XCTest
+
+final class Timer_with_ModUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
